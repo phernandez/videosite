c0cc73e38e8c682802c8a310f653d0748a03562e
Paul Hernandez <paulmh@gmail.com>
2012-07-02 07:30:30 -0500

    use flask app via settings

---
+++ video/settings.py
@@ -0,0 +1,15 @@
+'''
+Created on May 8, 2012
+
+@author: paul
+'''
+
+MONGOALCHEMY_SERVER = 'ds033047.mongolab.com'
+MONGOALCHEMY_PORT = '33047'
+MONGOALCHEMY_USER = 'video'
+MONGOALCHEMY_PASSWORD = 'video1'
+MONGOALCHEMY_DATABASE = 'video'
+MONGOALCHEMY_SERVER_AUTH = False
+
+SECRET_KEY = 'very secret, do you believe?'
+DEBUG = True
